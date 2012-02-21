77defd7f9cfe645143a38b3b57b425a0c7b1f020
scottjehl <scott@scottjehl.com>
2012-02-21 15:44:16 +0700

    all in.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Picturefill
+
+A crude polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
+
+* Author: Scott Jehl (c) 2012
+* License: MIT/GPLv2
+* Notes: For active discussion of the picture element, see [http://www.w3.org/community/respimg/](http://www.w3.org/community/respimg/). While this code does work, it is intended to be used only for example purposes until either:
+
+	A) A W3C Candidate Recommendation for <picture> is released
+		
+	B) A major browser implements <picture>
+
+Demo URL: [http://scottjehl.github.com/picturefill/](http://scottjehl.github.com/picturefill/)
+
+Note: The demo only polyfills picture support for browsers that support CSS3 media queries, but it includes the [matchMedia polyfill](https://github.com/paulirish/matchMedia.js/) for media-query-supporting browsers that don't have matchMedia.
---
+++ picturefill.js
@@ -0,0 +1,51 @@
+/*
+	Picturefill - a crude polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
+	* Author: Scott Jehl, 2012
+	* License: MIT/GPLv2
+	* Notes: 
+	  * For active discussion of the picture element, see http://www.w3.org/community/respimg/
+	  * While this code does work, it is intended to be used only for example purposes until either:
+		A) A W3C Candidate Recommendation for <picture> is released
+		B) A major browser implements <picture>
+*/ 
+(function( w ){
+	
+	// requires media query supporting browser with matchMedia support (polyfill available)
+	if( !w.matchMedia || !w.matchMedia( "only all" ) ){
+		return;
+	}
+	
+	w.picturefill = function(){
+		var ps = w.document.getElementsByTagName( "picture" );
+		
+		// Loop the pictures
+		for( var i = 0, il = ps.length; i < il; i++ ){
+			var sources = ps[ i ].getElementsByTagName( "source" ),
+				matches = [];
+				
+			// See if which sources match	
+			for( var j = 0, jl = sources.length; j < jl; j++ ){
+				if( w.matchMedia( sources[ j ].getAttribute( "media" ) ).matches ){
+					matches.push( sources[ j ] );
+				}
+			}
+
+			// Set fallback img element src from that of last matching source element
+			if( matches.length ){
+				ps[ i ].getElementsByTagName( "img" )[ 0 ].src =  matches.pop().getAttribute( "src" );
+			}
+		}
+	}
+	
+	// Run on resize
+	if( w.addEventListener ){
+		w.addEventListener( "resize", picturefill, false );
+	}
+	else if( w.addEvent ){
+		w.addEvent( "onresize", picturefill );
+	}
+	
+	// Run when DOM is ready
+	picturefill();
+	
+})( this );
