404c39d293a4ff95ee92ec6025841b9c5d9e7c1a
scottjehl <scott@scottjehl.com>
2012-03-22 18:12:33 +0530

    This refactors the proposed markup (slightly) and picturefill JS to broaden browser support. The included update to README.md explains the changes I'm proposing to the markup pattern (there no changes to elements themselves) to support browsers that formerly did not work, like iOS4, IE browsers versions 6-10, Android browsers 1.x and up. As a result, there aren't any known "unsupported" browsers, in that every browser tested so far at least receives a fallback image - for example, IE6-8 will only see picture source elements that do not use media queries, since it doesn't support them natively (media types are ok though). Fixes #5 and Fixes #8.

--- picturefill.js
+++ picturefill.js
@@ -1,7 +1,6 @@
+/*! Picturefill - Author: Scott Jehl, 2012 | License: MIT/GPLv2 */
 /*
-	Picturefill - a crude polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
-	* Author: Scott Jehl, 2012
-	* License: MIT/GPLv2
+	Picturefill: A polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
 	* Notes: 
 		* For active discussion of the picture element, see http://www.w3.org/community/respimg/
 		* While this code does work, it is intended to be used only for example purposes until either:
@@ -25,8 +24,19 @@
 		for( var i = 0, il = ps.length; i < il; i++ ){
 			var sources = ps[ i ].getElementsByTagName( "source" ),
 				matches = [];
+
+			// If no sources are found, they're likely erased from the DOM. Try finding them inside comments.
+			if( !sources.length ){
+				var picText =  ps[ i ].innerHTML,
+					frag = w.document.createElement( "div" ),
+					// For IE9, convert the source elements to divs
+					srcs = picText.replace( /(<)source([^>]+>)/gmi, "$1div$2" ).match( /<div[^>]+>/gmi );
 				
-			// See if which sources match	
+				frag.innerHTML = srcs.join( "" );
+				sources = frag.getElementsByTagName( "div" );
+			}
+			
+			// See if which sources match
 			for( var j = 0, jl = sources.length; j < jl; j++ ){
 				var media = sources[ j ].getAttribute( "media" );
 				// if there's no media specified, OR w.matchMedia is supported 
@@ -34,15 +44,17 @@
 					matches.push( sources[ j ] );
 				}
 			}
-
-			// Set fallback img element src from that of last matching source element
+			
 			if( matches.length ){
+				// Set fallback img element src from that of last matching source element
 				var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+			
 				if( !picImg ){
 					picImg = w.document.createElement( "img" );
 					picImg.alt = ps[ i ].getAttribute( "alt" );
 					ps[ i ].appendChild( picImg );
 				}
+				
 				picImg.src =  matches.pop().getAttribute( "src" );
 			}
 		}
