85357b481f835f9eee3f754cfa67055cf245f568
David Newton <david@davidnewton.ca>
2012-10-03 09:51:26 -0400

    Added a WordPress plugin
    WordPress automatically resizes images to create thumbnail, medium,
    large, and fullsize versions. The plugin loads these conditionally
    using picturefill. It also allows for SVG upload, and association of an
    SVG file with a normal raster image, so that the SVG may be included as
    a <source>

---
+++ wp-picturefill/picturefill.pictureelement.js
@@ -0,0 +1,193 @@
+/*! Picturefill - Author: Scott Jehl, 2012 | License: MIT/GPLv2 */
+/*
+	Picturefill: A polyfill for proposed behavior of the picture element, which does not yet exist, but should. :)
+	* Notes: 
+		* For active discussion of the picture element, see http://www.w3.org/community/respimg/
+		* While this code does work, it is intended to be used only for example purposes until either:
+			A) A W3C Candidate Recommendation for <picture> is released
+			B) A major browser implements <picture>
+*/ 
+(function( w ){
+
+	// Enable strict mode
+	"use strict";
+
+	// User preference for HD content when available
+	var prefHD = false || w.localStorage && w.localStorage[ "picturefill-prefHD" ] === "true",
+		hasHD;
+
+	// Test if `<picture>` is supported natively, if so, exit - no polyfill needed.
+	if ( !!( w.document.createElement( "picture" ) && w.document.createElement( "source" ) && w.HTMLPictureElement ) ){
+		return;
+	}
+
+	w.types = {};
+
+	w.picturedetect = function() {
+		// based on Modernizr's inlinesvg test
+		// https://github.com/Modernizr/Modernizr/blob/master/modernizr.js
+		var ns = {'svg': 'http://www.w3.org/2000/svg'},
+			div = document.createElement( "div" );
+		div.innerHTML = "<svg/>";
+		if( ( div.firstChild && div.firstChild.namespaceURI ) == ns.svg ){
+			w.types['image/svg+xml'] = true;
+		}
+
+		// based on Modernizr's img-webp test
+		// https://github.com/Modernizr/Modernizr/blob/master/feature-detects/img-webp.js
+		// note: asynchronous
+		var image = new Image();
+		image.onload = function() {
+			if( image.width == 1 ){
+				w.types['image/webp'] = true;
+			}
+		};
+		image.src = "data:image/webp;base64,UklGRiwAAABXRUJQVlA4ICAAAAAUAgCdASoBAAEAL/3+/3+CAB/AAAFzrNsAAP5QAAAAAA==";
+	};
+
+	w.picturefill = function() {
+		var ps = w.document.getElementsByTagName( "picture" );
+
+		// Loop the pictures
+		for( var i = 0, il = ps.length; i < il; i++ ){
+
+				var sources = ps[ i ].getElementsByTagName( "source" ),
+					picImg = null,
+					matches = [],
+					lastType;
+
+				// If no sources are found, they're likely erased from the DOM. Try finding them inside comments.
+				if( !sources.length ){
+					var picText =  ps[ i ].innerHTML,
+						frag = w.document.createElement( "div" ),
+						// For IE9, convert the source elements to divs
+						srcs = picText.replace( /(<)source([^>]+>)/gmi, "$1div$2" ).match( /<div[^>]+>/gmi );
+	
+					frag.innerHTML = srcs.join( "" );
+					sources = frag.getElementsByTagName( "div" );
+				}
+
+				// See which sources match
+				for( var j = 0, jl = sources.length; j < jl; j++ ){
+					var media = sources[ j ].getAttribute( "media" ),
+						type = sources[ j ].getAttribute("type");
+
+					// stop once type changes if we've already found matches
+					if( matches.length && type != lastType ){
+						break;
+					}
+
+					// if there's no type OR the type is in w.types
+					if (!type || ( w.types[type] == true) ){
+
+						// if there's no media specified, OR w.matchMedia is supported
+						if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
+							matches.push( sources[ j ] );
+						}
+					}
+
+					lastType = type;
+				}
+
+			// Find any existing img element in the picture element
+			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
+
+			if( matches.length ){
+				// Grab the most appropriate (last) match.
+				var match = matches.pop(),
+					srcset = match.getAttribute( "srcset" );
+
+				if( !picImg ){
+					picImg = w.document.createElement( "img" );
+					picImg.alt = ps[ i ].getAttribute( "alt" );
+					ps[ i ].appendChild( picImg );
+				}
+
+
+				if( srcset ) {
+						var screenRes = ( prefHD && w.devicePixelRatio ) || 1, // Is it worth looping through reasonable matchMedia values here?
+							sources = srcset.split(","); // Split comma-separated `srcset` sources into an array.
+
+						hasHD = w.devicePixelRatio > 1;
+
+						for( var res = sources.length, r = res - 1; r >= 0; r-- ) { // Loop through each source/resolution in `srcset`.
+							var source = sources[ r ].replace(/^\s*/, '').replace(/\s*$/, '').split(" "), // Remove any leading whitespace, then split on spaces.
+								resMatch = parseFloat( source[1], 10 ); // Parse out the resolution for each source in `srcset`.
+
+							if( screenRes >= resMatch ) {
+								if( picImg.getAttribute( "src" ) !== source[0] ) {
+									var newImg = document.createElement("img");
+
+									newImg.src = source[0];
+									// When the image is loaded, set a width equal to that of the original’s intrinsic width divided by the screen resolution:
+									newImg.onload = function() {
+										// Clone the original image into memory so the width is unaffected by page styles:
+										this.width = ( this.cloneNode( true ).width / resMatch );
+									}
+									picImg.parentNode.replaceChild( newImg, picImg );
+								}
+								break; // We’ve matched, so bail out of the loop here.
+							}
+						}
+				} else {
+					// No `srcset` in play, so just use the `src` value:
+					picImg.src = match.getAttribute( "src" );
+				}
+			}
+			else if( picImg ){
+				ps[ i ].removeChild( picImg );
+			}
+		}
+		/*
+// Manual resolution switching, to simulate UA interference.
+		if( hasHD ){
+			var body = w.document.getElementsByTagName("body")[0],
+				prevSwitch = w.document.getElementById( "#toggle-res" ),
+				picSwitch = w.document.createElement( "a" );
+
+			if( prevSwitch ){
+				body.removeChild( prevSwitch );
+			}
+
+			picSwitch.id = "toggle-res";
+			picSwitch.href = "#";
+			picSwitch.innerHTML = ( prefHD ? "S" : "H" ) + "D only";
+			picSwitch.title = "Switch images to " + ( prefHD ? "Standard" : "High" ) + "Definition";
+			picSwitch.className = "pf-pref pf-pref-" + ( prefHD ? "standard" : "high" );
+
+			body.insertBefore( picSwitch, body.children[0] );
+
+			picSwitch.onclick = function(){
+				prefHD = !prefHD;
+				if( w.localStorage ){
+					w.localStorage[ "picturefill-prefHD" ] = prefHD; 
+				}
+				return false;
+			};
+		}*/
+	};
+
+
+	// Run on resize and domready (w.load as a fallback)
+	if( w.addEventListener ){
+		w.addEventListener( "resize", w.picturefill, false );
+		w.addEventListener( "DOMContentLoaded", function(){
+			w.picturedetect();
+
+			// WebP detect is asynchronous
+			// this should allow it to finish before picturefill runs
+			setTimeout(w.picturefill, 100);
+
+			// Run once only
+			w.removeEventListener( "load", w.picturedetect, false );
+			w.removeEventListener( "load", w.picturefill, false );
+		}, false );
+		w.addEventListener( "load", w.picturedetect, false );
+		w.addEventListener( "load", w.picturefill, false );
+	}
+	else if( w.attachEvent ){
+		w.attachEvent( "onload", w.picturedetect );
+		w.attachEvent( "onload", w.picturefill );
+	}
+
+}( this ));
---
+++ wp-picturefill/wp-picturefill.php
@@ -0,0 +1,193 @@
+<?php
+
+/*
+Plugin Name: WP picturefill
+Description: Replaces <code>&lt;img&gt;</code> tags with responsive <code>&lt;picture&gt;</code> tags. Uses a <a rel="external" href="https://github.com/nwtn/picturefill">fork</a> of Scott Jehl's <a rel="external" href="https://github.com/scottjehl/picturefill">picturefill</a>.
+Version: 1.0
+Author: David Newton
+Author URI: http://davidnewton.ca
+*/
+
+
+add_action('wp_enqueue_scripts', 'wppf_script'); 
+add_filter('upload_mimes', 'wppf_upload_mimes');
+add_filter('the_content', 'wppf_content');
+add_filter("attachment_fields_to_edit", "wppf_svg_field_edit", null, 2);
+add_filter("attachment_fields_to_save", "wppf_svg_field_save", null, 2);
+
+if (!function_exists('get_attachment_id')) {
+    /**
+     * Get the Attachment ID for a given image URL.
+     * @link   http://wordpress.stackexchange.com/a/7094
+     * @param  string $url
+     * @return boolean|integer
+     */
+    function get_attachment_id($url) {
+        $dir = wp_upload_dir();
+
+        // baseurl never has a trailing slash
+        if (false === strpos($url, $dir['baseurl'] . '/')) {
+            // URL points to a place outside of upload directory
+            return false;
+        }
+
+        $file  = basename($url);
+        $query = array(
+            'post_type'  => 'attachment',
+            'fields'     => 'ids',
+            'meta_query' => array(
+                array(
+                    'value'   => $file,
+                    'compare' => 'LIKE',
+               ),
+           )
+       );
+
+        $query['meta_query'][0]['key'] = '_wp_attached_file';
+
+        // query attachments
+        $ids = get_posts($query);
+
+        if (! empty($ids)) {
+            foreach ($ids as $id) {
+                // first entry of returned array is the URL
+                if ($url === array_shift(wp_get_attachment_image_src($id, 'full')))
+                    return $id;
+            }
+        }
+
+        $query['meta_query'][0]['key'] = '_wp_attachment_metadata';
+
+        // query attachments again
+        $ids = get_posts($query);
+
+        if (empty($ids))
+            return false;
+
+        foreach ($ids as $id) {
+            $meta = wp_get_attachment_metadata($id);
+            foreach ($meta['sizes'] as $size => $values) {
+                if ($values['file'] === $file && $url === array_shift(wp_get_attachment_image_src($id, $size)))
+                    return $id;
+            }
+        }
+
+        return false;
+    }
+}
+
+if (!function_exists('wppf_upload_mimes')) {
+	function wppf_upload_mimes($existing_mimes=array()) {
+		$existing_mimes['svg'] = 'mime/type';
+		return $existing_mimes;
+	}
+}
+
+if (!function_exists('wppf_svg_field_edit')) {
+	/**
+	 * Adding our custom fields to the $form_fields array
+	 * @link http://net.tutsplus.com/tutorials/wordpress/creating-custom-fields-for-attachments-in-wordpress/
+	 * @param array $form_fields
+	 * @param object $post
+	 * @return array
+	 */
+	function wppf_svg_field_edit($form_fields, $post) {
+		if (substr($post->post_mime_type, 0, 5) == 'image' && $post->post_mime_type != 'image/svg+xml') {  
+			$form_fields["svg"] = array(
+				"label" => __("SVG file URL"),
+				"input" => "text", // this is default if "input" is omitted
+				"value" => get_post_meta($post->ID, "_svg", true)
+			);
+			return $form_fields;
+		}
+	}
+}
+
+if (!function_exists('wppf_svg_field_save')) {
+	/**
+	 * @link http://net.tutsplus.com/tutorials/wordpress/creating-custom-fields-for-attachments-in-wordpress/
+	 * @param array $post
+	 * @param array $attachment
+	 * @return array
+	 */
+	function wppf_svg_field_save($post, $attachment) {
+		if (isset($attachment['svg'])){
+			update_post_meta($post['ID'], '_svg', $attachment['svg']);
+		}
+		return $post;
+	}
+}
+
+if (!function_exists('wppf_script')) {
+	function wppf_script() {  
+		wp_register_script('picturefill', plugins_url( 'picturefill.pictureelement.js', __FILE__ ));
+		wp_enqueue_script('picturefill');
+	}
+}
+
+if (!function_exists('wppf_content')) {
+	function wppf_content($content) {
+		$content = preg_replace_callback('/<img[^>]+\>/i', "wppf_replace", $content);
+		return $content;
+	}
+}
+
+if (!function_exists('wppf_replace')) {
+	function wppf_replace($matches) {
+		$img = $matches[0];
+	
+		preg_match_all('/(id|class|src|alt|title)="([^"]*)"/i', $img, $matches);
+		$attributes = $matches[1];
+		$values = $matches[2];
+		
+		// make sure we have an image src AND that it's a WP attachment
+		$src = array_search('src', $attributes);
+		if ($src === false) { return $img; }
+		unset($attributes[$src]);
+		$src = $values[$src];
+		$attachmentID = get_attachment_id($src);
+		if (empty($attachmentID)) { return $img; }
+		
+		$attachment 				= wp_get_attachment_metadata($attachmentID);
+		$attachment_image			= wp_get_attachment_image_src($attachmentID, 'full');
+		$attachment_image_small		= wp_get_attachment_image_src($attachmentID, 'thumbnail');
+		$attachment_image_medium	= wp_get_attachment_image_src($attachmentID, 'medium');
+		$attachment_image_large		= wp_get_attachment_image_src($attachmentID, 'large');
+		
+		// picture tag
+		$output = '
+			<picture ';
+		foreach ($attributes as $key => $attribute) {
+			if ($values[$key] != '') {
+				$output .= $attribute . '="' . $values[$key] . '"';
+			}
+		}
+		$output .= '>';
+		
+		// svg
+		$svg = get_post_meta($attachmentID, '_svg');
+		if (!empty($svg)) {
+			$output .= '
+				<!-- If browser supports inline SVG, use image below: -->
+				<source type="image/svg+xml" src="' . $svg[0] . '">
+			';
+		}
+		
+		// responsive raster images and fallback
+		$output .= '			
+				<!-- Otherwise, fallback on rasters -->
+				<source srcset="' . $attachment_image_medium[0] . ' 1x, ' . $attachment_image_large[0] . ' 2x">
+				<source media="(min-width: 50em)" srcset="' . $attachment_image_large[0] . ' 1x, ' . $attachment_image[0] . ' 2x">
+				<source media="(min-width: 100em)" src="' . $attachment_image[0] . '">
+			
+				<!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+				<noscript>' . $img[0] . '</noscript>
+			</picture>
+		';
+		
+		return $output;
+	}
+}
+
+
+?>
