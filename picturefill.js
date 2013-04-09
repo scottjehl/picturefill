6b8f755ca9bcd74f8970c76a7806a66b897f2488
Nelson Menezes <nelson_menezes@yahoo.co.uk>
2013-04-09 10:49:01 +0300

    Use querySelectorAll if available

--- picturefill.js
+++ picturefill.js
@@ -6,23 +6,35 @@
 	"use strict";
 
 	w.picturefill = function() {
-		var ps = w.document.getElementsByTagName( "div" );
+		var ps = [],
+			psTemp;
+		
+		if (w.querySelectorAll) {
+			ps = w.querySelectorAll( "div[data-picture]" );
+		}
+		else {
+			psTemp = w.document.getElementsByTagName( "div" );
+			for( var i = 0, il = psTemp.length; i < il; i++ ){
+				if( psTemp[ i ].getAttribute( "data-picture" ) !== null ){
+					ps.push(psTemp[ i ])
+				}
+			}
+		}
 		
 		// Loop the pictures
 		for( var i = 0, il = ps.length; i < il; i++ ){
-			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
 
-				var sources = ps[ i ].getElementsByTagName( "div" ),
-					matches = [];
-			
-				// See if which sources match
-				for( var j = 0, jl = sources.length; j < jl; j++ ){
-					var media = sources[ j ].getAttribute( "data-media" );
-					// if there's no media specified, OR w.matchMedia is supported 
-					if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
-						matches.push( sources[ j ] );
-					}
+			var sources = ps[ i ].getElementsByTagName( "div" ),
+				matches = [];
+		
+			// See if which sources match
+			for( var j = 0, jl = sources.length; j < jl; j++ ){
+				var media = sources[ j ].getAttribute( "data-media" );
+				// if there's no media specified, OR w.matchMedia is supported 
+				if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
+					matches.push( sources[ j ] );
 				}
+			}
 
 			// Find any existing img element in the picture element
 			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
@@ -39,7 +51,6 @@
 			else if( picImg ){
 				ps[ i ].removeChild( picImg );
 			}
-		}
 		}
 	};
 	
