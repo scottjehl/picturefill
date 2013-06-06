2df5ff38fa1c953fa9116c0b220429a472dae0cb
Scott Jehl <scott@scottjehl.com>
2013-06-06 11:00:15 +0100

    whitespace only

--- picturefill.js
+++ picturefill.js
@@ -1,20 +1,20 @@
 /*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
 
 (function( w ){
-	
+
 	// Enable strict mode
 	"use strict";
 
 	w.picturefill = function() {
 		var ps = w.document.getElementsByTagName( "span" );
-		
+
 		// Loop the pictures
 		for( var i = 0, il = ps.length; i < il; i++ ){
 			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
 
 				var sources = ps[ i ].getElementsByTagName( "span" ),
 					matches = [];
-			
+
 				// See if which sources match
 				for( var j = 0, jl = sources.length; j < jl; j++ ){
 					var media = sources[ j ].getAttribute( "data-media" );
@@ -27,13 +27,13 @@
 			// Find any existing img element in the picture element
 			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
 
-			if( matches.length ){			
+			if( matches.length ){
 				if( !picImg ){
 					picImg = w.document.createElement( "img" );
 					picImg.alt = ps[ i ].getAttribute( "data-alt" );
 					ps[ i ].appendChild( picImg );
 				}
-				
+
 				picImg.src =  matches.pop().getAttribute( "data-src" );
 			}
 			else if( picImg ){
@@ -42,7 +42,7 @@
 		}
 		}
 	};
-	
+
 	// Run on resize and domready (w.load as a fallback)
 	if( w.addEventListener ){
 		w.addEventListener( "resize", w.picturefill, false );
@@ -56,5 +56,5 @@
 	else if( w.attachEvent ){
 		w.attachEvent( "onload", w.picturefill );
 	}
-	
+
 }( this ));
