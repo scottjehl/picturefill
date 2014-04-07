0a717c99233e08579eea29fafb8e2bc306faf697
Scott Jehl <scottjehl@gmail.com>
2014-04-07 16:28:07 -0500

    added html5shims for #149

--- src/picturefill.js
+++ src/picturefill.js
@@ -12,6 +12,10 @@
 		return;
 	}
 
+	// HTML shim|v it for old IE (IE9 will still need the HTML video tag workaround)
+	doc.createElement( "picture" );
+	doc.createElement( "source" );
+
 	// local object for method references and testing exposure
 	var pf = {};
 
