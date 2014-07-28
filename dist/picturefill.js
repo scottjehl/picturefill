2978c4abbc79214673a6209c974d26a81ee108ca
Christophe de Jonge <christophe@MacBook.local>
2014-07-28 16:55:47 +0200

    residual version change

--- dist/picturefill.js
+++ dist/picturefill.js
@@ -1,4 +1,4 @@
-/*! Picturefill - v2.1.0 - 2014-07-27
+/*! Picturefill - v2.1.0 - 2014-07-28
 * http://scottjehl.github.io/picturefill
 * Copyright (c) 2014 https://github.com/scottjehl/picturefill/blob/master/Authors.txt; Licensed MIT */
 /*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
