a6d15ab798ff5960a0ee423e7ce6e2fbd17ae8dc
Zoltan Hawryluk <zhawryluk@sapient.com>
2014-11-29 15:11:23 -0500

    Added grunt documentation to the index.html file/

--- dist/picturefill.js
+++ dist/picturefill.js
@@ -187,18 +187,6 @@ window.matchMedia || (window.matchMedia = function() {
 	pf.types[ "image/webp" ] = pf.detectImageSupport("image/webp", "UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=");
 	pf.types[ "image/vnd.ms-photo" ] = pf.detectImageSupport("image/vnd.ms-photo", "SUm8AQgAAAAFAAG8AQAQAAAASgAAAIC8BAABAAAAAQAAAIG8BAABAAAAAQAAAMC8BAABAAAAWgAAAMG8BAABAAAAHwAAAAAAAAAkw91vA07+S7GFPXd2jckNV01QSE9UTwAZAYBxAAAAABP/gAAEb/8AAQAAAQAAAA==");
 	pf.types[ "image/jp2" ] = pf.detectImageSupport("image/jp2", "/0//UQAyAAAAAAABAAAAAgAAAAAAAAAAAAAABAAAAAQAAAAAAAAAAAAEBwEBBwEBBwEBBwEB/1IADAAAAAEAAAQEAAH/XAAEQED/ZAAlAAFDcmVhdGVkIGJ5IE9wZW5KUEVHIHZlcnNpb24gMi4wLjD/kAAKAAAAAABYAAH/UwAJAQAABAQAAf9dAAUBQED/UwAJAgAABAQAAf9dAAUCQED/UwAJAwAABAQAAf9dAAUDQED/k8+kEAGvz6QQAa/PpBABr994EAk//9k=");
-	pf.types["image/x-apng"] = function() {
-		var apngTest = new Image(), ctx = document.createElement("canvas").getContext("2d");
-		apngTest.onload = function() {
-			ctx.drawImage(apngTest, 0, 0);
-			pf.types["image/x-apng"] = ctx.getImageData(0, 0, 1, 1).data[3] === 0;
-			picturefill();
-		};
-		apngTest.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACGFjVEwAAAABAAAAAcMq2TYAAAANSURBVAiZY2BgYPgPAAEEAQB9ssjfAAAAGmZjVEwAAAAAAAAAAQAAAAEAAAAAAAAAAAD6A+gBAbNU+2sAAAARZmRBVAAAAAEImWNgYGBgAAAABQAB6MzFdgAAAABJRU5ErkJggg==";
-		// frame 1 (skipped on apng-supporting browsers): [0, 0, 0, 255]
-		// frame 2: [0, 0, 0, 0];
-		return "pending";
-	}; 
 
 	/**
 	 * Takes a source element and checks if its type attribute is present and if so, supported
